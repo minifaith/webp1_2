23d4316343b69c320403f9f4e90d251703315476
maxfaith <vdovytska@gmail.com>
2020-03-11 20:42:52 +0200

    Initial commit

---
+++ src/webp1/Main.java
@@ -0,0 +1,10 @@
+package webp1;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+        Message message = new Message ();
+
+        System.out.println(message.getMess());
+    }
+}
---
+++ src/webp1/Message.java
@@ -0,0 +1,14 @@
+package webp1;
+
+public class Message {
+    private String mess = "Hello world";
+
+    public String getMess() {
+        return mess;
+    }
+
+    public void setMess(String mess) {
+        this.mess = mess;
+    }
+
+}
